4ca991714799080d44188ce06550b37c79ff115a
mohamad <mohamad.gharib@ut.ee>
2022-10-18 22:38:33 +0300

    1st

---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <to-do-list/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import ToDoList from "@/components/ToDoList.vue";   
+
+export default {
+  name: "HomeView",
+  components: {
+    ToDoList
+  },
+};
+</script>
