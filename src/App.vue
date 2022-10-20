ba697761acff26a8b744f6ed9ac026a95abb230b
mohamad <mohamad.gharib@ut.ee>
2022-10-20 10:04:07 +0300

    2nd

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
+    <router-link to="/form">Form Val.</router-link> |
+    <router-link to="/fetch">FetchAPI</router-link> |
+    <router-link to="/faq">FAQ</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
   <router-view />
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -15,3 +15,17 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+body {
+    position: relative;
+}
+
+.post {
+    width: 80%;
+    position: relative;
+    padding: 10px;
+    margin: 10px auto;
+    border: 1px solid gray;
+}
+</style>
